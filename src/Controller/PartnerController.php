6ba60126009f048888edecd8c6d5889d3186ccd9
Piotr <piotr.tchernobilski@gmail.com>
2022-07-15 16:18:14 +0200

    Test PartnerController, et affichage

--- src/Controller/PartnerController.php
+++ src/Controller/PartnerController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Partner;
+use App\Repository\PartnerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class PartnerController extends AbstractController
 {
     #[Route('/partner', name: 'app_partner')]
-    public function index(): Response
+    public function index(PartnerRepository $partnerRepository): Response
     {
+        $partners = $partnerRepository->findAll();
+        $activePartners = $partnerRepository->findBy(['is_active' => true]);
+        $notActivePartners = $partnerRepository->findBy(['is_active' => false]);
+
+
         return $this->render('partner/index.html.twig', [
-            'controller_name' => 'PartnerController',
+            'partners' => $partners,
+            'activePartners' => $activePartners,
+            'notActivePartners' => $notActivePartners
         ]);
     }
 }
--- src/Controller/StructureController.php
+++ src/Controller/StructureController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\StructureRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class StructureController extends AbstractController
 {
     #[Route('/structure', name: 'app_structure')]
-    public function index(): Response
+    public function index(StructureRepository $structureRepository): Response
     {
+        $structures = $structureRepository->findAll();
+        $activeStructures = $structureRepository->findBy(['is_active' => true]);
+        $notActiveStructures = $structureRepository->findBy(['is_active' => false]);
         return $this->render('structure/index.html.twig', [
-            'controller_name' => 'StructureController',
+            'structures' => $structures,
+            'activeStructures' => $activeStructures,
+            'notActiveStructures' => $notActiveStructures
         ]);
     }
 }
