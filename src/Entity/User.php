0b3c29f807a080e53507c0fc93d7ebe91ac03728
tpichon <tpichon@optilian.com>
2022-10-15 16:19:00 +0200

    Mise à jours du style - Header - Footer - Home

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -193,4 +193,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function __toString(): string
+    {
+        return $this->first_name . " " . $this->last_name;
+    }   
 }
