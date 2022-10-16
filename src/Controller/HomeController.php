21c85360663e3893fbafafa703b3cc2bc71d5fec
tpichon <tpichon@optilian.com>
2022-10-16 20:23:23 +0200

    Home - Intégration Maquettes - V1

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\PartnerRepository;
+use App\Repository\StructureRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,14 +11,27 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(PartnerRepository $partnerRepository): Response
+    public function index(PartnerRepository $partnerRepository, StructureRepository $structureRepository): Response
     {
+        $structuresNumber = count($structureRepository->findAll());
+        $activeStructuresNumber = count($structureRepository->findBy(['is_active' => true]));
+        $activeStructures = count($structureRepository->findBy(['is_active' => true]));
+        $noActiveStructures = count($structureRepository->findBy(['is_active' => false]));
+        $partnersNumber = count($partnerRepository->findAll());
+        $activePartnersNumber = count($partnerRepository->findBy(['is_active' => true]));
         $activePartners = $partnerRepository->findBy(['is_active' => true]);
         $notActivePartners = $partnerRepository->findBy(['is_active' => false]);
         return $this->render('home/index.html.twig', [
             'partners' => $partnerRepository->findAll(),
+            'structures' => $structureRepository->findAll(),
             'activePartners' => $activePartners,
-            'notActivePartners' => $notActivePartners
+            'notActivePartners' => $notActivePartners,
+            'activeStructures' => $activeStructures,
+            'noActiveStructures' => $noActiveStructures,
+            'partnersNumber' => $partnersNumber,
+            'activePartnersNumber' => $activePartnersNumber,
+            'structuresNumber' => $structuresNumber,
+            'activeStructuresNumber' => $activeStructuresNumber
         ]);
     }
 }
